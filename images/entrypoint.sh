35f2d8f438074417207884fdcf1da2c5286005e3
cyclinder <qifeng.guo@daocloud.io>
2022-08-22 16:27:02 +0800

    fix image can't works and reduce the size of image

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 function log()
 {
     echo "$(date --iso-8601=seconds) ${1}"
 }
 
+cp /home/* /host/opt/cni/bin/
+ls /host/opt/cni/bin
+
 log "Entering sleep (success)..."
 sleep infinity
-
