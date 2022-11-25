26b602f22fc5e7c47419a9535e1e03b29e23d8c9
Roronoa-Z3r0 <ji.li@daocloud.io>
2022-11-25 14:35:59 +0800

    add spiderdoctor overlay e2e

--- test/e2e/macvlan-overlay-one/macvlan_overlay_one_test.go
+++ test/e2e/macvlan-overlay-one/macvlan_overlay_one_test.go
@@ -2,89 +2,75 @@ package macvlan_overlay_one_test
 
 import (
 	"context"
-	"fmt"
+	"errors"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/spidernet-io/cni-plugins/test/e2e/common"
-	"net"
-	"os"
+	apitypes "k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("MacvlanOverlayOne", Label("overlay", "one-nic"), func() {
-	It("Host can be communicate with pod, including pod in same and different node", Label("ping"), func() {
-		for _, node := range frame.Info.KindNodeList {
-			for _, podIP := range podIPs {
-				command := common.GetPingCommandByIPFamily(podIP)
-				_, err := frame.DockerExecCommand(context.TODO(), node, command)
-				Expect(err).NotTo(HaveOccurred(), " host %s failed to ping %s: %v,", node, podIP, err)
-			}
-		}
-	})
 
-	It("Pods in different node can be communicate with each other", Label("ping"), func() {
-		for _, pod := range podList.Items {
-			for _, podIP := range podIPs {
-				command := common.GetPingCommandByIPFamily(podIP)
-				_, err := frame.ExecCommandInPod(pod.Name, pod.Namespace, command, context.TODO())
-				Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("pod %s/%s failed to %s", pod.Namespace, pod.Name, command))
-			}
-		}
-	})
+	It("spiderdoctor connectivity should be succeed", Label("spiderdoctor"), func() {
+		// create task spiderdoctor crd
+		task.Name = name
+		// schedule
+		plan.StartAfterMinute = 0
+		plan.RoundNumber = 2
+		plan.IntervalMinute = 2
+		plan.TimeoutMinute = 2
+		task.Spec.Schedule = plan
+		// target
+		targetAgent.TestIngress = true
+		targetAgent.TestEndpoint = true
+		targetAgent.TestClusterIp = true
+		targetAgent.TestMultusInterface = true
+		targetAgent.TestNodePort = true
+		targetAgent.TestIPv4 = &common.IPV4
+		// TODO https://github.com/projectcalico/calico/issues/6877
+		targetAgent.TestIPv6 = &testIPv6
 
-	// ping calico pod ?
+		target.TargetAgent = targetAgent
+		task.Spec.Target = target
+		// request
+		request.DurationInSecond = 5
+		request.QPS = 1
+		request.PerRequestTimeoutInSecond = 15
 
-	It("remote's client should be access to pod", Label("ping"), func() {
-		// get remote's client container name
-		vlanGatewayName := os.Getenv(common.ENV_VLAN_GATEWAY_CONTAINER)
-		Expect(vlanGatewayName).NotTo(BeEmpty(), "failed to get env 'VLAN_GATEWAY_CONTAINER'")
+		task.Spec.Request = request
+		// success condition
 
-		for _, podIP := range podIPs {
-			command := common.GetPingCommandByIPFamily(podIP)
-			_, err := frame.DockerExecCommand(context.TODO(), vlanGatewayName, command)
-			Expect(err).To(Succeed())
-		}
-	})
+		condition.SuccessRate = &successRate
+		condition.MeanAccessDelayInMs = &delayMs
 
-	It("Pod should be access to clusterIP, including ipv4 and ipv6", Label("curl"), func() {
-		for _, pod := range podList.Items {
-			for _, clusterIP := range clusterIPs {
-				// TODO https://github.com/projectcalico/calico/issues/6877
-				if net.ParseIP(clusterIP).To4() == nil {
-					continue
+		task.Spec.SuccessCondition = condition
+
+		taskCopy := task
+		GinkgoWriter.Printf("spiderdoctor task: %+v", task)
+		err := frame.CreateResource(task)
+		Expect(err).NotTo(HaveOccurred(), " spiderdoctor nethttp crd create failed")
+
+		err = frame.GetResource(apitypes.NamespacedName{Name: name}, taskCopy)
+		Expect(err).NotTo(HaveOccurred(), " spiderdoctor nethttp crd get failed")
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*60*5)
+		defer cancel()
+		for run {
+			select {
+			case <-ctx.Done():
+				run = false
+				Expect(errors.New("wait nethttp test timeout")).NotTo(HaveOccurred(), " running spiderdoctor task timeout")
+			default:
+				err = frame.GetResource(apitypes.NamespacedName{Name: name}, taskCopy)
+				Expect(err).NotTo(HaveOccurred(), " spiderdoctor nethttp crd get failed")
+				if taskCopy.Status.Finish == true {
+					for _, v := range taskCopy.Status.History {
+						Expect(v.Status).To(Equal("succeed"), "round %d failed", v.RoundNumber)
+					}
+					run = false
 				}
-				command := common.GetCurlCommandByIPFamily(clusterIP, 80)
-				output, err := frame.ExecCommandInPod(pod.Name, pod.Namespace, command, context.TODO())
-				Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("pod %s/%s failed to %s: %s \n", pod.Namespace, pod.Name, command, output))
-			}
-		}
-	})
-
-	It("Host should be access to clusterIP, including ipv4 and ipv6", Label("curl"), func() {
-		for _, node := range frame.Info.KindNodeList {
-			for _, clusterIP := range clusterIPs {
-				// TODO https://github.com/projectcalico/calico/issues/6877
-				if net.ParseIP(clusterIP).To4() == nil {
-					continue
-				}
-				command := common.GetCurlCommandByIPFamily(clusterIP, 80)
-				GinkgoWriter.Printf("docker exec -it %s %s \n", node, command)
-				output, err := frame.DockerExecCommand(context.TODO(), node, command)
-				Expect(err).NotTo(HaveOccurred(), " host %s failed to access to cluster service %s: %s,", node, clusterIP, output)
-			}
-		}
-
-	})
-	It("Host should be access to nodePort address, including ipv4 and ipv6", Label("curl"), func() {
-		for _, node := range frame.Info.KindNodeList {
-			for _, nodeIP := range nodeIPs {
-				// TODO https://github.com/projectcalico/calico/issues/6877
-				if net.ParseIP(nodeIP).To4() == nil {
-					continue
-				}
-				command := common.GetCurlCommandByIPFamily(nodeIP, nodePorts[0])
-				GinkgoWriter.Printf("docker exec -it %s %s \n", node, command)
-				output, err := frame.DockerExecCommand(context.TODO(), node, command)
-				Expect(err).NotTo(HaveOccurred(), " host %s failed to access to nodePort service %s/%d: %s \n", node, nodeIP, nodePorts[0], output)
+				time.Sleep(time.Second * 5)
 			}
 		}
 	})
--- test/e2e/macvlan-overlay-two/macvlan_overlay_two_suite_test.go
+++ test/e2e/macvlan-overlay-two/macvlan_overlay_two_suite_test.go
@@ -1,7 +1,18 @@
 package macvlan_overlay_two_test
 
 import (
+	"context"
+	"fmt"
+	multus_v1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	"github.com/spidernet-io/cni-plugins/pkg/schema"
+	"github.com/spidernet-io/cni-plugins/test/e2e/common"
+	e2e "github.com/spidernet-io/e2eframework/framework"
+	"github.com/spidernet-io/e2eframework/tools"
+	spiderdoctorV1 "github.com/spidernet-io/spiderdoctor/pkg/k8s/apis/spiderdoctor.spidernet.io/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -11,3 +22,72 @@ func TestMacvlanOverlayTwo(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "MacvlanOverlayTwo Suite")
 }
+
+var frame *e2e.Framework
+var name string
+var spiderDoctorAgent *appsv1.DaemonSet
+var annotations = make(map[string]string)
+var successRate = float64(1)
+var delayMs = int64(10000)
+var testIPv6 = false
+
+var (
+	task        *spiderdoctorV1.Nethttp
+	plan        *spiderdoctorV1.SchedulePlan
+	target      *spiderdoctorV1.NethttpTarget
+	targetAgent *spiderdoctorV1.TargetAgentSepc
+	request     *spiderdoctorV1.NethttpRequest
+	condition   *spiderdoctorV1.NetSuccessCondition
+	run         = true
+)
+
+var _ = BeforeSuite(func() {
+	defer GinkgoRecover()
+	var e error
+	task = new(spiderdoctorV1.Nethttp)
+	plan = new(spiderdoctorV1.SchedulePlan)
+	target = new(spiderdoctorV1.NethttpTarget)
+	targetAgent = new(spiderdoctorV1.TargetAgentSepc)
+	request = new(spiderdoctorV1.NethttpRequest)
+	condition = new(spiderdoctorV1.NetSuccessCondition)
+
+	frame, e = e2e.NewFramework(GinkgoT(), []func(*runtime.Scheme) error{multus_v1.AddToScheme, schema.SpiderPoolAddToScheme, spiderdoctorV1.AddToScheme})
+	Expect(e).NotTo(HaveOccurred())
+
+	name = "two-macvlan-overlay-" + tools.RandomName()
+
+	// get macvlan-overlay multus crd instance by name
+	multusInstance, err := frame.GetMultusInstance(common.MacvlanOverlayVlan0Name, common.MultusNs)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(multusInstance).NotTo(BeNil())
+
+	multusInstance, err = frame.GetMultusInstance(common.MacvlanOverlayVlan100Name, common.MultusNs)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(multusInstance).NotTo(BeNil())
+
+	annotations[common.MultusAddonAnnotation_Key] = fmt.Sprintf("%s/%s,%s/%s", common.MultusNs, common.MacvlanOverlayVlan0Name, common.MultusNs, common.MacvlanOverlayVlan100Name)
+	annotations[common.SpiderPoolIPPoolsAnnotationKey] = `[{"interface": "net1", "ipv4": ["default-v4-ippool"], "ipv6": ["default-v6-ippool"]},{"interface": "net2", "ipv4": ["vlan100-v4"], "ipv6": ["vlan100-v6"]}]`
+
+	GinkgoWriter.Printf("update spiderdoctoragent annotation: %v/%v annotation: %v \n", common.SpiderDoctorAgentNs, common.SpiderDoctorAgentDSName, annotations)
+	spiderDoctorAgent, err = frame.GetDaemonSet(common.SpiderDoctorAgentDSName, common.SpiderDoctorAgentNs)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(spiderDoctorAgent).NotTo(BeNil())
+
+	spiderDoctorAgent.Spec.Template.Annotations = annotations
+	err = frame.UpdateResource(spiderDoctorAgent)
+	Expect(err).NotTo(HaveOccurred())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*common.CtxTimeout)
+	defer cancel()
+	nodeList, err := frame.GetNodeList()
+	Expect(err).NotTo(HaveOccurred())
+	err = frame.WaitPodListRunning(spiderDoctorAgent.Spec.Selector.MatchLabels, len(nodeList.Items), ctx)
+	Expect(err).NotTo(HaveOccurred())
+
+	time.Sleep(10 * time.Second)
+})
+
+var _ = AfterSuite(func() {
+	err := frame.DeleteResource(task)
+	Expect(err).NotTo(HaveOccurred(), "failed to delete spiderdoctor nethttp %v", name)
+})
